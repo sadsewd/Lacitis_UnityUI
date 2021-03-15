55d66ad4e371bc8dfb160c98e38cbb9b378a6998
sadsewd <[lacitiskarlis@gmail.com]>
2021-03-15 14:24:39 +0200

    Pievienoti ievades logi kuros pieprasa apģermjamā tēla vārdu un vecumu (vecumam var ievadit tikai skaitļus) tālāk tika izveidota poga un skripts kurš uzņem ievaddatus un aizsuta tos uz teksta lauku. Skripta metodi kas izvada datus tiek palaists ar pogas palīdzību

---
+++ Assets/Skripti/DatuIzvade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DatuIzvade : MonoBehaviour
+{
+
+    public string vecums;
+    public string teksts;
+    public GameObject vecumaLauks;
+    public GameObject tekstaLauks;
+    public GameObject tekstaAttelosana;
+
+    public void uzglabatTekstu()
+    {
+        vecums = vecumaLauks.GetComponent<Text>().text;
+        teksts = tekstaLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = teksts+" ir "+vecums+" gadus vecs!";
+    }
+
+}
