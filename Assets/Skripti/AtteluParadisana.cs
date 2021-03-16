2a1a59f57cd8bf54dbecf54b15571738073d4aaa
sadsewd <[lacitiskarlis@gmail.com]>
2021-03-16 14:35:00 +0200

    Tika pievienots izkrītošais saraksts ar kuru tiek mainīts apģērbjamais tēls

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour
+{
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+    
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        }
+        else if (skaitlis == 1)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        }
+    }
+}
